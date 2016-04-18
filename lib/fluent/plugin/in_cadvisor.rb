f4b1d910e3a1d95cad0759fdf255625561700ae2
Steffen Prohaska <prohaska@zib.de>
2016-04-18 10:54:38 +0200

    Use cAdvisor API v1.3 by default
    I only test with the latest cAdvisor.  Bump the API version accordingly.

--- lib/fluent/plugin/in_cadvisor.rb
+++ lib/fluent/plugin/in_cadvisor.rb
@@ -24,7 +24,7 @@ class CadvisorInput < Fluent::Input
   config_param :tag, :string
   config_param :host, :string, :default => 'localhost'
   config_param :port, :string, :default => 8080
-  config_param :api_version, :string, :default => '1.2'
+  config_param :api_version, :string, :default => '1.3'
   config_param :api_variant, :string, :default => 'container'
   config_param :stats_interval, :time, :default => 60 # every minute
   config_param :docker_url, :string,  :default => 'unix:///var/run/docker.sock'
